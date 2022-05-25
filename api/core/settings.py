ebbe0dcd444f823e3e4dcb2d4c4744d6dd3d363b
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-05-24 22:24:44 -0500

    move secret key to environment variable

--- api/core/settings.py
+++ api/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_i0_v$^2@ej#oq(n&g6q-9n)c3o9fq(now-fmeo)alhq-von$k'
+SECRET_KEY = environ.get('SECRET_KEY', '!!! SECRET KEY !!!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
