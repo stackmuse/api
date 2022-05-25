c388a8c3c5d7bdba4ef45360780753e86cc5b282
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-05-25 18:34:34 -0500

    map jobs views on router

--- api/core/urls.py
+++ api/core/urls.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 
+from jobs.views import CompetencyViewSet, JobViewSet
+
 router = routers.DefaultRouter()
+router.register('jobs', JobViewSet)
+router.register('competencies', CompetencyViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
